fe0c30f7a49c0962a097d5ec277efe9a54b58283
pinktaty <diaz.lilith04@gmail.com>
2024-01-24 18:57:48 -0600

    check cors problem

---
+++ assets/scrpits/index.js
@@ -0,0 +1,35 @@
+function loadLanguage(xhr, lan){
+    let data = JSON.parse(xhr.responseText);
+    document.querySelector(".welcome").innerHTML = data.welcome;
+    location.hash = lan;
+    location.reload();
+}
+
+function changeLanguage(lan){
+    switch (lan) {
+        case "es":
+            const es  = new XMLHttpRequest();
+            es.open('GET', './assets/json/espaniol.json', true);
+            es.send();
+            es.onload = function () {
+                loadLanguage(es, "es");
+            }
+            break;
+        case "en":
+            const en  = new XMLHttpRequest();
+            en.open('GET', './assets/json/english.json', true);
+            en.send();
+            en.onload = function () {
+                loadLanguage(en, "en");
+            }
+            break;
+        case "no":
+            const no  = new XMLHttpRequest();
+            no.open('GET', './assets/json/norsk.json', true);
+            no.send();
+            no.onload = function () {
+                loadLanguage(no, "no");
+            }
+            break;
+    }
+}
