72f7d6c9bc14dd646e483aa2ecef9ac04f0210c8
pinktaty <diaz.lilith04@gmail.com>
2024-01-24 19:40:13 -0600

    mejorar escritura código

--- assets/scrpits/index.js
+++ assets/scrpits/index.js
@@ -1,35 +1,30 @@
-function loadLanguage(xhr, lan){
-    let data = JSON.parse(xhr.responseText);
+var language = new XMLHttpRequest();
+var hash;
+
+function loadLanguage(){
+    let data = JSON.parse(language.responseText);
     document.querySelector(".welcome").innerHTML = data.welcome;
-    location.hash = lan;
+    location.hash = hash;
 }
 
 function changeLanguage(lan){
     switch (lan) {
         case "es":
-            const es  = new XMLHttpRequest();
-            es.open('GET', './assets/json/espaniol.json', true);
-            es.send();
-            es.onload = function () {
-                loadLanguage(es, "es");
-            }
+            language.open('GET', './assets/json/espaniol.json', true);
+            hash = "es";
             break;
         case "en":
-            const en  = new XMLHttpRequest();
-            en.open('GET', './assets/json/english.json', true);
-            en.send();
-            en.onload = function () {
-                loadLanguage(en, "en");
-            }
+            language.open('GET', './assets/json/english.json', true);
+            hash = "en";
             break;
         case "no":
-            const no  = new XMLHttpRequest();
-            no.open('GET', './assets/json/norsk.json', true);
-            no.send();
-            no.onload = function () {
-                loadLanguage(no, "no");
-            }
+            language.open('GET', './assets/json/norsk.json', true);
+            hash = "no"
             break;
+    }
+    language.send();
+    language.onload = function () {
+        loadLanguage();
     }
 }
 
